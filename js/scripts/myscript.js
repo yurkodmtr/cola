8d7805fe4982a60dfef47346521735de15c5d516
yurko <4au.4au.4au@gmail.com>
2018-10-09 17:56:03 +0300

    1

--- js/scripts/myscript.js
+++ js/scripts/myscript.js
@@ -2,13 +2,27 @@
 
 var myFunc = function(){
 
+	var anim = function(){
+		var animation = setInterval( moveSprite,50);
+		var index=0;
+		function moveSprite(){		 
+			var nextSlide = index+1;
+			$('.anim img:nth-child('+index+')').hide();	   
+			$('.anim img:nth-child('+nextSlide+')').show();					
+		    index++;
+		    if(index === 150){
+		        clearInterval(animation);
+		        //$('.loader').fadeOut();
+		    }
+		}
+	}
 
 	$(document).ready(function(){
 
 	});
 
 	$(window).load(function(){
-
+		anim();
 	});
 
 }
